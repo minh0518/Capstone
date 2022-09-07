839c5f909600e7d27515e892c0e136749d8f3c02
minh0518 <rubics@naver.com>
2022-09-07 20:43:56 +0900

    category

--- src/Home.js
+++ src/Home.js
@@ -3,25 +3,64 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Home = () => {
-  const [koficInfo, setkoficInfo] = useState([])
+  const [koficInfo, setKoficInfo] = useState([])
   const [naverInfo, setNaverInfo] = useState([])
+  const [nationCategory,setNationCategories]=useState('')
+  const [multiCategory,setMultiCategorie]=useState('')
+
+  
+const nationCategories = [
+  {
+    name: 'ALL',
+    text: '',
+  },
+  {
+    name: '한국영화',
+    text: 'K',
+  },
+  {
+    name: '외국영화',
+    text: 'F',
+  },
+]
+
+const multiCategories=[
+  {
+    name: 'ALL',
+    text: '',
+  },
+  {
+    name: '다양성 영화',
+    text: 'Y',
+  },
+  {
+    name: '상업 영화',
+    text: 'N',
+  }
+]
+
+
   
   //영화진흥위원회 정보
   useEffect(() => {
     const getMovies = async () => {
       try {
+
+        const nationQuery= nationCategory? `&repNationCd=${nationCategory}` : ''
+        const multiQuery=multiCategory?`&multiMovieYn=${multiCategory}` :''
         let response = await axios.get(
-          'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=a5ff207b0075a51ed12c42d6c4b67218&targetDt=20220905',
+          `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=a5ff207b0075a51ed12c42d6c4b67218&targetDt=20220905${nationQuery}${multiQuery}`,
         )
+        console.log(nationQuery)
 
         let list = response.data.boxOfficeResult.dailyBoxOfficeList
-        setkoficInfo(list)
+        setKoficInfo(list)
       } catch (e) {
         console.log(e)
       }
     }
     getMovies()
-  }, [])
+  },[nationCategory,multiCategory])
 
   let NaverInfoArr = []
 
@@ -58,6 +97,22 @@ const Home = () => {
   return (
     <div>
       <h1>Select Movie</h1>
+
+
+    <ul>
+      {nationCategories.map((i,index)=>{
+        return(
+              <li key={index}><button onClick={()=>{setNationCategories(i.text)}}>{i.name}</button></li>         
+        )
+      })}
+    </ul>
+    <ul>
+      {multiCategories.map((i,index)=>{
+        return(
+              <li key={index}><button onClick={()=>{setMultiCategorie(i.text)}}>{i.name}</button></li>         
+        )
+      })}
+    </ul>
 
       {naverInfo.map((i, index) => {
         return (
