8a817fcff152c004ba81e1ce7f8b3f15628398b5
minh0518 <rubics@naver.com>
2022-10-26 03:07:26 +0900

    개인프로필 BestPick 추가,삭제 버그픽스

--- src/components/profile/MyProfile.js
+++ src/components/profile/MyProfile.js
@@ -18,7 +18,11 @@ const Profile = ({ userObj }) => {
   })
 
   const [bestPickValue, setBestPickValue] = useState('')
+  //bestPick을 추가할때 input창에 작성한 값
+
+
   const [bestPickArr, setBestPickArr] = useState('')
+  //bestPick을 새로 추가or삭제하기 직전의 값들을 담고 있음
 
   const genre = {
     ALL: '',
@@ -132,23 +136,34 @@ const Profile = ({ userObj }) => {
       })
     }
 
-    console.log([...bestPickArr, bestPickValue])
+    setBestPickArr([...bestPickArr, bestPickValue])
+    //bestPickArr은 최초에 db에서 받아오는건 맞는데
+    //그 이후에는 db를 다시 엑세스 하지 않으므로 (useEffect는 한번)
+    //업뎃을 하면 그 값으로 bestPickArr를 수정해 줘야 한다
+
+
 
     //화면에 수정된 값을 바로 보여주기 위해 일부러 추가
     //닉네임,생년월일 이런 것들은 input에 입력하면
     //바로 setProfile의 상태들을 onChange로 바꿔줘서
-    //수정을 완료하면 화면에 수정된 값이 그대로 보인다
-    //(사실 이건 DB에 저장된 값을 다시 새로 불러와서 보여주는 것이 아니다
-    // 그냥 현재 상태값을 보여줄 뿐)
-    //근데 bestPick은 아니므로 어쩔 수 없이 편법으로 상태값을 또 한번
-    //수정을 해준다 그러면 화면에 수정된 값이 바로 보인다
+    //수정을 완료하면 화면에 수정된 profile상태 값으로써 바로 보인다
+
+    //그러므로 우리가 논리적으로 생각하는
+    //"아 화면에 보이는 이정보들은 DB에서 가져온 것이구나" 가 아니다
+    // 그냥 현재 상태값을 보여 주는 것이다
+    //근데 profile상태 중에서 bestPick속성은 onChange에서 입력하면서 
+    //바로 상태를 바꾸지 않으므로 어쩔 수 없이 여기서 편법으로 
+    //bestPick부분의 상태값을 또 한번 수정을 해준다
     setProfile((prev) => ({
       ...prev,
       bestPick: [...bestPickArr, bestPickValue]
     }))
     //고질적인 문제인데 강제로 화면을 리렌더링해서 다시 useEffect를
     //그려내는 방법은 없는건가?
-    //그리고 이렇게 하면 또 마지막 값이 자동으로 삭제가 되는 오류가 발생한다
+    //물론 여기서 useEffect의 의존성 배열을 없애면 되지만
+    //그러면 무한 리렌더링이라 이것만은 진짜 하면 안될 것 같다
+
+    
 
     setBestPickValue('')
     setEditMode((prev) => !prev)
@@ -176,6 +191,13 @@ const Profile = ({ userObj }) => {
       ...prev,
       bestPick: arr
     }))
+
+
+    setBestPickArr(arr)
+     //bestPickArr은 최초에 db에서 받아오는건 맞는데
+    //그 이후에는 db를 다시 엑세스 하지 않으므로 (useEffect는 한번)
+    //삭제를 하면 삭제를 하고 난 다음의 DB값으로 
+    //bestPickArr를 수정해 줘야 한다
 
     setEditMode((prev) => !prev)
   }
